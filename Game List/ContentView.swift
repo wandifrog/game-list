f078b49dde55ec5387171bcb6da1dfbaba527763
Wandi <wandifrog@gmail.com>
2021-08-10 19:45:04 +0700

    Initial Commit

---
+++ Game List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Game List
+//
+//  Created by Wandi on 10/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Game List/Game_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_ListApp.swift
+//  Game List
+//
+//  Created by Wandi on 10/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Game_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
